db1d3158bc8e9d21344e2d8d65cec558c07d8013
Prashant R <smplsoln@gmail.com>
2021-10-28 01:05:58 -0600

    Add Apppointment in Application JSX

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "components/Application.scss";
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -22,9 +23,67 @@ const days = [
   },
 ];
 
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  console.log({day});
+  console.log({ day });
+
+
+  const appointmentComponents = appointments.map(
+    appointment => {
+      return  (
+      <Appointment
+        key={appointment.id}
+        // id={appointment.id}
+        // time={appointment.time}
+        // interview={appointment.interview}
+        {...appointment}
+      />);
+    });
+
+  appointmentComponents.push(<Appointment key="last" time="5pm" />);
+
+  console.log({appointmentComponents});
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +107,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentComponents}
       </section>
     </main>
   );
