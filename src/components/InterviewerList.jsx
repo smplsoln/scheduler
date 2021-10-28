427799822779f9285f4c338206dc487b2358acc1
Prashant R <smplsoln@gmail.com>
2021-10-27 18:14:53 -0600

    Remove interviewer.id from the InterviewerListItem props, update JSX and storybook

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+  console.log("InterviewerList props", {props});
+  const interviewerListItems = [];
+
+  for (const interviewer of props.interviewers) {
+    interviewerListItems.push(<InterviewerListItem
+      key = {interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={props.interviewer === interviewer.id}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+    />);
+  }
+
+  return <section className="interviewers">
+    <h4 className="interviewers__header text--light">Interviewer</h4>
+    <ul className="interviewers__list"> {interviewerListItems} </ul>
+  </section>
+}
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -8,28 +8,17 @@ import 'components/InterviewerListItem.scss';
 
 export default function InterviewListItem(props) {
 
-  console.log({props});
-  const id = props.id;
+  console.log("InterviewerListItem props", {props});
   const avatar = props.avatar;
   const selected = props.selected;
   const name = props.selected ? props.name : "";
-  const handleClick = props.setInterviewer
-    ? (() => {
-      const retVal = props.setInterviewer(id);
-      console.log("setInterviewer: ", id, " return: ", retVal);
-      return retVal;
-    })
-    : (() => {
-      console.log("Interviewer selected: ", {id});
-      return id;
-    });
 
   const itemClasses = classNames("interviewers__item ", {
     "interviewers__item--selected": selected,
   })
 
   console.log({itemClasses});
-  return <li className={itemClasses} key={id} onClick={handleClick}>
+  return <li className={itemClasses} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
