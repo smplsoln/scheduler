0d6deae3f64f62d2fefa517790091bee55b717b3
Prashant R <smplsoln@gmail.com>
2021-10-29 02:36:20 -0600

    Add combined promise all GET requests for days, appointments, interviewers and set init state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,50 +5,12 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+import { getAppointmentsForDay } from "helpers/selectors";
 
 const apiServer = "http://localhost:8001";
-const appointmentsUrl = "/api/appointments";
-const daysUrl = "/api/days";
-const interviewersUrl = "/api/interviewers"
+const daysUrl = apiServer + "/api/days";
+const appointmentsUrl = apiServer + "/api/appointments";
+const interviewersUrl = apiServer + "/api/interviewers"
 
 export default function Application(props) {
 
@@ -63,51 +25,52 @@ export default function Application(props) {
       appointments: {}
     }
   );
+  console.log({ state });
 
-  console.log({state});
+  useEffect(() => {
+    Promise.all([
+      axios.get(daysUrl),
+      axios.get(appointmentsUrl),
+      axios.get(interviewersUrl),
+    ])
+    .then((all) => {
+      console.log("GET all results: ", { all });
+      setState((prevState) => ({
+        ...prevState,
+        days: [...all[0].data],
+        appointments: { ...all[1].data },
+        interviewers: { ...all[2].data }
+      }));
+    })
+    // [] dependency arr for running this
+    // only once at start/first time
+  }, []);
+
+  let dailyAppointments = [];
 
   // set day in state
   const setDay = (dayName) => {
     setState((prevState) => {
-      return {...prevState, day: dayName};
+      return { ...prevState, day: dayName };
     });
   };
 
-  console.log( "Days: ", state.days );
+  console.log("Days: ", state.days);
 
-  useEffect(() => {
-    const url = apiServer + daysUrl;
-    console.log("GET ",{url});
-    axios.get(url)
-    .then((res) => {
-      console.log({res});
-      const data = res.data;
-      console.log("result of GET ", {url}, {data});
-
-      setState((prevState) => {
-        return {...prevState, days: [...data]};
-      });
-    })
-    .catch((err) => {
-      console.log("error in GET ",{url}, {err});
-    })
-  }, []) // [] dependency arr means only once at start/first time
-
-  const appointmentComponents = appointments.map(
+  dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointmentComponents = dailyAppointments.map(
     appointment => {
-      return  (
-      <Appointment
-        key={appointment.id}
-        // id={appointment.id}
-        // time={appointment.time}
-        // interview={appointment.interview}
-        {...appointment}
-      />);
+      return (
+        <Appointment
+          key={appointment.id}
+          // id={appointment.id}
+          // time={appointment.time}
+          // interview={appointment.interview}
+          {...appointment}
+        />);
     });
-
   appointmentComponents.push(<Appointment key="last" time="5pm" />);
-
-  console.log({appointmentComponents});
+  console.log({ appointmentComponents });
 
   return (
     <main className="layout">
